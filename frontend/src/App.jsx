1c420fc592aff8aebe084c61d78ec54ef8867553
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-10-19 16:03:20 +0530

    chore: finally create a base setup using React.js + Tailwind CSS + Vite.js

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import "./index.css";
+
+const App = () => {
+  return (
+    <div className="flex items-center justify-center text-3xl font-bold h-screen bg-pink-400 text-white">
+      Hello Sakshi!
+    </div>
+  );
+};
+
+export default App;
