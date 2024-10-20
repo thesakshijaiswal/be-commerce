b8e95f10ded40425f918112d7988dd7eab7fcab0
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2024-10-20 10:51:34 +0530

    feat: create header layout for large screen devices

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,9 @@
+import Header from "./components/Header";
 import "./index.css";
 
 const App = () => {
   return (
-    <div className="flex items-center justify-center text-3xl font-bold h-screen bg-pink-400 text-white">
-      Hello Sakshi!
-    </div>
+    <Header />
   );
 };
 
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <nav className="bg-[#FFB703] p-4">
+      <div className="">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center">
+            <img src="../../public/logo.svg" alt="BE-commerce" className="w-20"/>
+            <a href="/" className="text-2xl font-bold">
+              BE-commerce
+            </a>
+          </div>
+          <div>
+          <input type="text" placeholder="Search" className="p-2 w-96 rounded-lg" />
+          <button className="text-white bg-black px-4 py-2 rounded-lg ml-2">Search</button>
+          </div>
+          <div>
+            <ul className="flex gap-5 font-semibold">
+              <li>Cart</li>
+              <li>Profile</li>
+              <li>Login</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
