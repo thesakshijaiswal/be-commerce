fd4da43d93f5466959b997e9f28393eb4ea61850
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-11-03 15:09:45 +0530

    feat: create pages folder and add two different routes one for home and another for ProductDetails that uses dynamic routing i.e. useParams

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,11 +7,18 @@ import {
 } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
+import Home from "./pages/Home.jsx";
+import ProductDetails from "./pages/ProductDetails.jsx";
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route path="/" element={<App />}></Route>)
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<Home />} />
+      <Route path="/product/:id" element={<ProductDetails />} />
+    </Route>,
+  ),
 );
 
 createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
+  <RouterProvider router={router} />,
 );
---
+++ frontend/src/pages/ProductDetails.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+const ProductDetails = () => {
+  const { id } = useParams();
+  console.log(id);
+  return <div>ProductDetails</div>;
+};
+
+export default ProductDetails;
