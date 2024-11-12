b33c8d510b9d3fa1894b5935ea2ebde10f7041a4
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2024-11-12 16:11:04 +0530

    feat: add tilt card effect to product card using framer motion hooks

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,11 +1,54 @@
+import { motion, useMotionValue, useSpring, useTransform } from "framer-motion";
 const ProductCard = ({ image, name }) => {
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
+
+  const mouseXSpring = useSpring(x);
+  const mouseYSpring = useSpring(y);
+
+  const rotateX = useTransform(
+    mouseYSpring,
+    [-0.5, 0.5],
+    ["18.5deg", "-18.5deg"],
+  );
+  const rotateY = useTransform(
+    mouseXSpring,
+    [-0.5, 0.5],
+    ["18.5deg", "-18.5deg"],
+  );
+  const handleMouseMove = (e) => {
+    const rect = e.target.getBoundingClientRect();
+    const width = rect.width;
+    const height = rect.height;
+    const mouseX = e.clientX - rect.left;
+    const mouseY = e.clientY - rect.right;
+    const xPerct = mouseX / width - 0.5;
+    const yPerct = mouseY / height - 0.5;
+
+    x.set(xPerct);
+    y.set(yPerct);
+  };
+  const handleMouseLeave = () => {
+    x.set(0);
+    y.set(0);
+  };
   return (
-    <div className="h-96 w-80 rounded-lg bg-[#323235] p-4 duration-300 hover:scale-105">
-      <div className="h-56 w-full overflow-hidden rounded-md">
+    <motion.div
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseLeave}
+      style={{ rotateX, rotateY, transformStyle: "preserve-3d" }}
+      className="h-96 w-80 rounded-lg bg-[#323235] p-4 duration-300 hover:scale-105"
+    >
+      <div
+        style={{ transformStyle: "preserve-3d" }}
+        className="h-56 w-full overflow-hidden rounded-md"
+      >
         <img className="h-full w-full" src={image} alt={name} />
       </div>
-      <h2 className="mt-2 truncate text-lg">{name}</h2>
-    </div>
+      <h2 className="mt-3 truncate text-lg font-semibold leading-tight text-white">
+        {name}
+      </h2>
+    </motion.div>
   );
 };
 
