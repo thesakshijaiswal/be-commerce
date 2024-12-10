316270f5d026cf0113345e88274468dddac95262
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2024-12-10 18:23:21 +0530

    feat: create a read more functionality for product description

--- frontend/src/pages/ProductDetails.jsx
+++ frontend/src/pages/ProductDetails.jsx
@@ -2,21 +2,32 @@ import { Link, useParams } from "react-router-dom";
 import products from "../data/products";
 import Button from "../components/Button";
 import { TiArrowBackOutline } from "react-icons/ti";
+import { useState } from "react";
 
 const ProductDetails = () => {
   const { id } = useParams();
+  const product = products.find((product) => product?.id === Number(id));
+  const [isExpanded, setIsExpanded] = useState(false);
+  const toggleRead = () => setIsExpanded((prevState) => !prevState);
   return (
     <div className="mt-28 flex min-h-screen justify-center gap-6">
       <div className="w-96">
-        <img
-          className="rounded-lg"
-          src={products.find((product) => product?.id === Number(id)).image}
-          alt="product-image"
-        />
+        <img className="rounded-lg" src={product.image} alt="product-image" />
       </div>
-      <span className="w-1/3 text-base font-semibold text-white">
-        {products.find((product) => product?.id === Number(id)).name}
-      </span>
+      <div className="w-1/3 space-y-4 text-base text-black">
+        <p className="font-semibold">{product.name}</p>
+        <p>
+          {isExpanded
+            ? product.description
+            : `${product.description.slice(0, 200)}... `}
+          <span
+            onClick={toggleRead}
+            className="cursor-pointer font-semibold text-primary"
+          >
+            {isExpanded ? "Read Less" : "Read More"}
+          </span>
+        </p>
+      </div>
       <Link to="/">
         <Button className="bg-primary" btnIcon={TiArrowBackOutline}>
           Back
