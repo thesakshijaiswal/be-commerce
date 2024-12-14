1bc4b5e2eca4daec03643fa6a46a002831857ed6
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-12-14 16:50:44 +0530

    feat: move data folder to backend and create first api route for products using express server

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,16 @@
-import mongoose from "mongoose";
 import express from "express";
+import products from "./data/products.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("api is running...");
+});
+
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
 
 app.listen(PORT, () => {
   console.log("Server is running on " + PORT);
