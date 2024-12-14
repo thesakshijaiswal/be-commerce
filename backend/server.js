52742afa4f292aee7b47aed68ab8c16ef1e10836
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-12-14 17:23:43 +0530

    api: get dynamic data based on request id params

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,13 @@ app.get("/api/products", (req, res) => {
   res.json(products);
 });
 
+//Getting dynamic route data
+app.get("/api/products/:id", (req, res) => {
+  // console.log(req.params.id);
+  const product = products.find((product) => product.id == req.params.id); //req.params.id is a string hence double equal only
+  res.json(product);
+});
+
 app.listen(PORT, () => {
   console.log("Server is running on " + PORT);
 });
