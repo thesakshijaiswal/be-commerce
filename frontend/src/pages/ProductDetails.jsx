c770cb9820667089310624a3b2e90e22b7cea188
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2024-12-22 19:17:43 +0530

    feat: enhance product details page with quantity selector and add to cart functionality

--- frontend/src/pages/ProductDetails.jsx
+++ frontend/src/pages/ProductDetails.jsx
@@ -1,12 +1,14 @@
 import { Link, useParams } from "react-router-dom";
 import Button from "../components/Button";
 import { TiArrowBackOutline } from "react-icons/ti";
-import { BsCurrencyRupee } from "react-icons/bs";
+import { BsCurrencyRupee, BsCartPlus } from "react-icons/bs";
+import { GrMoney } from "react-icons/gr";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { StarRating } from "../components";
 
 const ProductDetails = () => {
+  const [quantity, setQuantity] = useState(1);
   const [product, setProduct] = useState({});
   const { id } = useParams();
   useEffect(() => {
@@ -21,7 +23,7 @@ const ProductDetails = () => {
   const toggleRead = () => setIsExpanded((prevState) => !prevState);
 
   return (
-    <div className="mt-16 flex min-h-screen flex-col items-center px-4 sm:px-10 md:px-3 lg:px-16">
+    <div className="mt-16 flex min-h-screen flex-col items-center p-4 sm:px-10 md:px-3 lg:px-16">
       <div className="flex w-full max-w-6xl flex-col gap-6 md:flex-row lg:items-start">
         <div className="flex w-full items-center justify-center lg:w-1/2">
           <div className="w-80 rounded-lg bg-white p-2 shadow-md lg:w-96">
@@ -34,8 +36,13 @@ const ProductDetails = () => {
         </div>
         <div className="flex w-full flex-col space-y-4 text-black lg:w-1/2">
           <h1 className="text-lg font-semibold sm:text-xl">{product.name}</h1>
-          <p className="font-semibold text-base text-primary">Manufactured by {product.brand}</p>
-          <div><StarRating starRating={product.starRating} /></div>
+          <p className="text-base font-semibold text-primary">
+            Manufactured by {product.brand}
+          </p>
+          <div className="flex items-center gap-2">
+            <StarRating starRating={product.starRating} />|
+            <p className="font-medium text-primary">{product.rating} ratings</p>
+          </div>
           <p className="text-base">
             {isExpanded
               ? product.description
@@ -47,15 +54,57 @@ const ProductDetails = () => {
               {isExpanded ? "Read Less" : "Read More"}
             </span>
           </p>
+          <div className="text-lg">
+            {product.countInStock > 1 ? (
+              <p className="text-green-600">In Stock</p>
+            ) : (
+              <p className="text-red-600">Out of Stock</p>
+            )}
+          </div>
           <div className="flex items-center text-xl font-bold">
             <BsCurrencyRupee />
             <h3>{product.price}</h3>
           </div>
+          <div className="flex gap-2">
+            <Button
+              onClick={() => setQuantity((prev) => Math.max(1, prev - 1))}
+              disabled={quantity <= 1}
+            >
+              -
+            </Button>
+            <input
+              className="w-20 rounded-md bg-secondary/10 text-center"
+              id="quantity"
+              type="number"
+              value={quantity}
+              min="1"
+              max={product.countInStock}
+              onChange={(e) =>
+                setQuantity(
+                  Math.min(product.countInStock, Math.max(1, +e.target.value)),
+                )
+              }
+            />
+            <Button
+              onClick={() =>
+                setQuantity((prev) => Math.min(product.countInStock, prev + 1))
+              }
+              disabled={quantity >= product.countInStock}
+            >
+              +
+            </Button>
+          </div>
+          <div className="flex gap-4 pt-3">
+            <Button className="w-52 whitespace-nowrap">
+              Add to Cart <BsCartPlus className="text-xl" />
+            </Button>
+            <Button className="w-52 whitespace-nowrap">
+              Buy Now <GrMoney className="text-xl" />
+            </Button>
+          </div>
         </div>
         <Link to="/">
-          <Button className="bg-primary" btnIcon={TiArrowBackOutline}>
-            Back
-          </Button>
+          <Button btnIcon={TiArrowBackOutline}>Back</Button>
         </Link>
       </div>
     </div>
