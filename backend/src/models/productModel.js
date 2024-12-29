bbaf2014a20e249b5c48e42bc2814226eed70fc6
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2024-12-29 22:06:38 +0530

    feat: add MongoDB connection and product model schema

---
+++ backend/src/models/productModel.js
@@ -0,0 +1,49 @@
+import mongoose from " mongoose";
+const productSchema = new mongoose.schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    reviews: [reviewSchema],
+    ratings: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    numReviews: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    countInStock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
