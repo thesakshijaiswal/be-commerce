40b5e27320c194357a6aa241856f42ff478882f7
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2025-01-07 19:07:19 +0530

    feat: implement product controller and refactor product routes for improved structure

---
+++ backend/src/controllers/productController.js
@@ -0,0 +1,21 @@
+import Products from "../models/productModel.js";
+const asyncHandler = (fn) => (req, res, next) => {
+  Promise.resolve(fn(req, res, next)).catch(next);
+};
+
+const getProducts = asyncHandler(async (req, res) => {
+  const products = await Products.find({});
+  res.json(products);
+});
+
+const getProductsById = asyncHandler(async (req, res) => {
+  const product = await Products.findById(req.params.id);
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
+
+export { getProducts, getProductsById };
--- backend/src/routes/productRoute.js
+++ backend/src/routes/productRoute.js
@@ -1,31 +1,12 @@
 import express from "express";
-import Products from "../models/productModel.js";
+import {
+  getProducts,
+  getProductsById,
+} from "../controllers/productController.js";
+
 const router = express.Router();
 
-const asyncHandler = (fn) => (req, res, next) => {
-  Promise.resolve(fn(req, res, next)).catch(next);
-};
-
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    const products = await Products.find({});
-    res.json(products);
-  })
-);
-
-// Getting dynamic route data
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    const product = await Products.findById(req.params.id);
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404);
-      throw new Error("Product not found");
-    }
-  })
-);
+router.route("/").get(getProducts);
+router.route("/:id").get(getProductsById);
 
 export default router;
