0c58895fd1aa592b243f6e151ab565fe2332632c
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2025-01-08 17:39:04 +0530

    feat: replace useEffect and axios by RTK query for fetching products from backend

--- frontend/src/components/ProductCardContainer.jsx
+++ frontend/src/components/ProductCardContainer.jsx
@@ -1,24 +1,27 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import ProductCard from "./ProductCard";
-import axios from "axios";
+import { useGetProductsQuery } from "../features/productsApiSlice";
+
 const ProductCardContainer = () => {
-  const [products, setProducts] = useState([]);
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get("/api/products");
-      setProducts(data);
-    };
-    fetchProducts();
-  }, []);
+  const { data: products, isLoading, error } = useGetProductsQuery();
+
   return (
-    <div className="flex flex-wrap justify-center gap-x-5 gap-y-8">
-      {products.map((product) => (
-        <Link to={`product-details/${product?.id}`} key={product?.id}>
-          <ProductCard {...product} />
-        </Link>
-      ))}
-    </div>
+    <>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : error ? (
+        <div>{error?.data?.message || error?.error}</div>
+      ) : (
+        <div className="flex flex-wrap justify-center gap-x-5 gap-y-8">
+          {products.map((product) => (
+            <Link to={`product-details/${product?._id}`} key={product?._id}>
+              <ProductCard {...product} />
+            </Link>
+          ))}
+        </div>
+      )}
+    </>
   );
 };
 
