dad47264bf9e2e51cfbea2ba1b0cb297e6be87dd
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2025-01-08 15:57:16 +0530

    feat: add apiSlice for API interactions and integrate with Redux store

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,3 +1,3 @@
-const BASE_URL = "";
-const BACKEND_URL = "http://localhost:5000";
-const PRODUCTS_URL = "/api/products";
+export const BASE_URL = "";
+export const BACKEND_URL = "http://localhost:5000";
+export const PRODUCTS_URL = "/api/products";
