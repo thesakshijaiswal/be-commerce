4034faada1c5a4dc99c768a2d394b5d810d0067d
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2025-01-20 16:14:42 +0530

    feat: enhance CartPage layout with item details and styling improvements

--- frontend/src/pages/CartPage.jsx
+++ frontend/src/pages/CartPage.jsx
@@ -1,5 +1,6 @@
 import Button from "../components/Button";
 import { useSelector } from "react-redux";
+import { BsCurrencyRupee } from "react-icons/bs";
 const CartPage = () => {
   const { cartItems } = useSelector((state) => {
     return state.cart;
@@ -8,13 +9,31 @@ const CartPage = () => {
     <div className="flex flex-col items-start justify-center text-black md:flex-row">
       <div className="p-4 md:w-2/3">
         <h2 className="mb-4 text-2xl font-semibold">Shopping Cart</h2>
-        <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+        <div className="grid grid-cols-1 gap-4 lg:grid-cols-2">
           {cartItems.map((item) => {
             return (
               <div
-                className="flex items-center border border-gray-300 p-4"
+                className="items-center rounded-lg bg-primary/5 p-4 lg:flex"
                 key={item._id}
-              ></div>
+              >
+                <div className="mr-4 h-48 w-52 object-cover">
+                  <img
+                    src={item.image}
+                    alt={item.name}
+                    className="h-full w-full"
+                  />
+                </div>
+                <div className="flex flex-col gap-2 lg:w-1/2">
+                  <h3 className="text-base font-semibold">{item.name}</h3>
+                  <div className="flex items-center font-medium">
+                    <BsCurrencyRupee />
+                    <p className="font-medium text-gray-600">
+                      {item.price.toFixed(2)}
+                    </p>
+                  </div>
+                  <Button className="sm: w-52 bg-red-600">Remove</Button>
+                </div>
+              </div>
             );
           })}
         </div>
