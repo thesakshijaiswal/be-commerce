06f9a29478e9c489dd1342d03ac36b83483cb81c
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2025-01-23 16:27:20 +0530

    feat: implement JWT token generation and cookie handling in user login

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,10 +1,20 @@
 import User from "../models/user.model.js";
 import asyncHandler from "../utils/helper.js";
+import jwt from "jsonwebtoken";
 const userLogin = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
+  const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+    expiresIn: "20d",
+  });
 
-  if (user && await user.matchPassword(password)) {
+  res.cookie("jwt", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== "development",
+    sameSite: "strict",
+    maxAge: 20 * 24 * 60 * 60 * 1000, // 20 days
+  });
+  if (user && (await user.matchPassword(password))) {
     res.json({
       _id: user._id,
       name: user.name,
