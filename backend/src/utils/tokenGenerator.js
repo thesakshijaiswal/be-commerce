c62e917cecd3e4f565bf57a15a3940a4c7509303
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2025-01-24 18:28:43 +0530

    refactor: extract token generation and cookie handling into a separate utility function

---
+++ backend/src/utils/tokenGenerator.js
@@ -0,0 +1,14 @@
+import jwt from "jsonwebtoken";
+const tokenGenerator = (res, userId) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "20d",
+  });
+
+  res.cookie("jwt", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== "development",
+    sameSite: "strict",
+    maxAge: 20 * 24 * 60 * 60 * 1000, // 20 days
+  });
+};
+export default tokenGenerator;
