ca170a3996f426f3e9a06a828ac5c4797ff5b9b9
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2025-01-25 17:35:13 +0530

    feat: add userSignUp function to handle user registration

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -17,5 +17,27 @@ const userLogin = asyncHandler(async (req, res) => {
     throw new Error("Invalid email or password");
   }
 });
+const userSignUp = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+  const isUserExist = await User.findOne({ email });
+  if (isUserExist) {
+    res.status(400);
+    throw new error("User already exist");
+  }
 
-export { userLogin };
+  const user = await User.create({ name, email, password });
+  if (user) {
+    tokenGenerator(res, user._id);
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user credentials");
+  }
+});
+
+export { userLogin, userSignUp };
