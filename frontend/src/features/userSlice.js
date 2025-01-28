40791c54619f4970e90ace2b09192e2545192b75
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2025-01-28 18:07:16 +0530

    feat: create userSlice for managing user state and credentials

---
+++ frontend/src/features/userSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  userInfo: localStorage.getItem("userInfo")
+    ? JSON.parse(localStorage.getItem("userInfo"))
+    : null,
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    setCredentials: (state, action) => {
+      state.userInfo = action.payload;
+      localStorage.setItem("userInfo", JSON.stringify(action.payload));
+      const expirationDate = new Date() + 20 * 24 * 60 * 60 * 1000; // 20 days
+      localStorage.setItem("expirationDate", expirationDate);
+    },
+  },
+});
+
+export const { setCredentials } = userSlice.actions;
+export default userSlice.reducer;
