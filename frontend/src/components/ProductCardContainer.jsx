ab72e185238f7694755ff8312829735689f1ece4
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2025-03-05 19:06:12 +0530

    fix: enhance user authentication check in ProductCardContainer component

--- frontend/src/components/ProductCardContainer.jsx
+++ frontend/src/components/ProductCardContainer.jsx
@@ -6,13 +6,13 @@ import toast from "react-hot-toast";
 import { useEffect } from "react";
 import axios from "axios";
 import { BACKEND_URL } from "../utils/constants";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setCredentials } from "../features/userSlice";
 
 const ProductCardContainer = () => {
   const { data: products, isLoading, error } = useGetProductsQuery();
-
   const dispatch = useDispatch();
+  const user = useSelector((state) => state.user);
 
   const getUser = async () => {
     try {
@@ -31,13 +31,17 @@ const ProductCardContainer = () => {
         throw new Error("User not authenticated");
       }
     } catch (error) {
-      toast.error(error?.response?.data?.message || error.message);
+      if (error.response?.status !== 403) {
+        toast.error(error?.response?.data?.message || error.message);
+      }
     }
   };
 
   useEffect(() => {
-    getUser();
-  }, []);
+    if (!user || !user._id) {
+      getUser();
+    }
+  }, [user]);
 
   return (
     <>
