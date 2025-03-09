ba09861018976d6ab70beb36a40e5ff18ab4b28c
Kiran Mahajan <thekiranmahajan@outlook.com>
2025-03-09 12:39:08 +0530

    fix: simplify CORS configuration and enhance production handling in index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,23 +14,12 @@ import path from "path";
 
 connectDB();
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 const __dirname = path.resolve();
-
-app.use((req, res, next) => {
-  console.log("Request: log from index.js", req.method, req.url);
-  next();
-});
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  next();
-});
 
 app.use(
   cors({
-    origin: ["http://127.0.0.1:3000", "http://localhost:3000"],
-    methods: "GET, POST, PATCH, DELETE, PUT",
+    origin: "http://localhost:3000",
     credentials: true,
   })
 );
@@ -38,17 +27,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 configurePassport(app);
-
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
-
-app.use("/api/products", productRoute);
-app.use("/api/users", userRoutes);
-app.use("/auth", authRoutes);
-
-app.use(pathNotFound);
-app.use(errorHandler);
 
 /*********PRODUCTION CODE**********/
 if (process.env.NODE_ENV === "production") {
@@ -60,6 +38,17 @@ if (process.env.NODE_ENV === "production") {
 }
 /*********PRODUCTION CODE**********/
 
+app.get("/", (req, res) => {
+  res.send("Server is up and running 😌");
+});
+
+app.use("/api/products", productRoute);
+app.use("/api/users", userRoutes);
+app.use("/auth", authRoutes);
+
+app.use(pathNotFound);
+app.use(errorHandler);
+
 app.listen(PORT, () => {
-  console.log("Server is running on " + PORT);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
