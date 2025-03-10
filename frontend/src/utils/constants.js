586fb8f017c2d7ba88ab7953ce1c4384b94b73bb
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2025-03-10 16:43:45 +0530

    fix: update BASE_URL to point to the production server

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "";
+export const BASE_URL = "https://be-commerce-q7nw.onrender.com/";
 export const BACKEND_URL = "http://localhost:5000";
 export const PRODUCTS_URL = "/api/products";
 export const USERS_URL = "/api/users";
