b2db7229c98e89783e2d8ae4586fa990126a5dca
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2025-03-10 19:47:45 +0530

    style: enhance shipping page styling and update backend url in constants

--- frontend/src/pages/ShippingPage.jsx
+++ frontend/src/pages/ShippingPage.jsx
@@ -3,6 +3,7 @@ import { AuthLayout, Button, InputField } from "../components";
 import { FaRegAddressCard } from "react-icons/fa";
 import { PiCityDuotone, PiMailbox } from "react-icons/pi";
 import { SlLocationPin } from "react-icons/sl";
+import checkoutBanner from "../assets/checkout-Banner.svg";
 
 const ShippingPage = () => {
   const [address, setAddress] = useState("");
@@ -15,51 +16,56 @@ const ShippingPage = () => {
   };
 
   return (
-    <AuthLayout title="Shipping Information">
-      <form action="#" onSubmit={handleSubmit}>
-        <InputField
-          type="text"
-          fieldName="address"
-          placeholder="123 Main Street"
-          label="Address"
-          icon={FaRegAddressCard}
-          value={address}
-          onChange={(e) => setAddress(e.target.value)}
-        />
-        <InputField
-          type="text"
-          fieldName="city"
-          placeholder="Amravati"
-          label="City"
-          icon={PiCityDuotone}
-          value={city}
-          onChange={(e) => setCity(e.target.value)}
-        />
-        <InputField
-          type="text"
-          fieldName="postalCode"
-          placeholder="223012"
-          label="Postal Code"
-          icon={PiMailbox}
-          value={postalCode}
-          onChange={(e) => setPostalCode(e.target.value)}
-        />
-        <InputField
-          type="text"
-          fieldName="country"
-          placeholder="India"
-          label="Country"
-          icon={SlLocationPin}
-          value={country}
-          onChange={(e) => setCountry(e.target.value)}
-        />
-        <div className="pt-4">
-          <Button className="mb-2 w-full text-sm font-medium" type="submit">
-            Continue To Payment
-          </Button>
-        </div>
-      </form>
-    </AuthLayout>
+    <div className="flex max-h-screen items-center justify-center font-ubuntu">
+      <AuthLayout title="Shipping Information">
+        <form action="#" onSubmit={handleSubmit}>
+          <InputField
+            type="text"
+            fieldName="address"
+            placeholder="123 Main Street"
+            label="Address"
+            icon={FaRegAddressCard}
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+          />
+          <InputField
+            type="text"
+            fieldName="city"
+            placeholder="Amravati"
+            label="City"
+            icon={PiCityDuotone}
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+          />
+          <InputField
+            type="text"
+            fieldName="postalCode"
+            placeholder="223012"
+            label="Postal Code"
+            icon={PiMailbox}
+            value={postalCode}
+            onChange={(e) => setPostalCode(e.target.value)}
+          />
+          <InputField
+            type="text"
+            fieldName="country"
+            placeholder="India"
+            label="Country"
+            icon={SlLocationPin}
+            value={country}
+            onChange={(e) => setCountry(e.target.value)}
+          />
+          <div className="pt-4">
+            <Button className="mb-2 w-full text-sm font-medium" type="submit">
+              Continue To Payment
+            </Button>
+          </div>
+        </form>
+      </AuthLayout>
+      <div className="hidden w-full items-center justify-center md:flex md:w-2/5">
+        <img src={checkoutBanner} className="lg:h-[40rem]" alt="SignUpBanner" />
+      </div>
+    </div>
   );
 };
 
