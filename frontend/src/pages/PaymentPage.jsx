8456658c2d6d668c4dc5f1b1230a8661457d8da2
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2025-03-14 19:22:21 +0530

    feat: add custom radio buttons for payment options in PaymentPage

--- frontend/src/pages/PaymentPage.jsx
+++ frontend/src/pages/PaymentPage.jsx
@@ -1,13 +1,14 @@
 import { useSelector } from "react-redux";
 import { BsCurrencyRupee } from "react-icons/bs";
+import { FaStripe, FaRegCreditCard } from "react-icons/fa6";
+import "../index.css";
 
 const PaymentPage = () => {
   const { cartItems, taxPrice, shippingPrice, totalPrice } = useSelector(
-    (state) => {
-      return state.cart;
-    },
+    (state) => state.cart,
   );
   const totalQuantity = cartItems.reduce((sum, item) => sum + item.quantity, 0);
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-center px-3 sm:px-7 md:px-2">
       <h1 className="mb-6 ml-4 text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
@@ -15,17 +16,33 @@ const PaymentPage = () => {
       </h1>
       <div className="flex h-full w-full flex-col items-center justify-center gap-10 md:flex-row">
         <form action="#" className="flex h-96 w-full flex-col gap-10 lg:w-2/5">
-          <div className="flex h-1/2 w-full items-center gap-2 rounded-md bg-secondary/5 pl-12 shadow-md md:pl-24">
-            <input type="radio" name="payment" checked />
-            <label className="text-base font-semibold text-secondary md:text-lg">
-              Pay with Stripe
-            </label>
+          <div className="flex h-1/2 w-full items-center justify-around gap-2 rounded-md bg-secondary/5 shadow-md">
+            <div>
+              <label className="custom-radio">
+                <input type="radio" name="payment" defaultChecked />
+                <span className="checkmark"></span>
+              </label>
+              <label className="text-base font-semibold text-secondary md:text-lg">
+                Pay with Stripe
+              </label>
+            </div>
+            <span className="text-violet-500">
+              <FaStripe className="text-7xl" />
+            </span>
           </div>
-          <div className="flex h-1/2 w-full items-center gap-2 rounded-md bg-secondary/5 pl-12 shadow-md md:pl-24">
-            <input type="radio" name="payment" />
-            <label className="text-base font-semibold text-secondary md:text-lg">
-              Pay with Credit Card
-            </label>
+          <div className="flex h-1/2 w-full items-center justify-around gap-2 rounded-md bg-secondary/5 shadow-md">
+            <div>
+              <label className="custom-radio">
+                <input type="radio" name="payment" />
+                <span className="checkmark"></span>
+              </label>
+              <label className="text-base font-semibold text-secondary md:text-lg">
+                Pay with Credit Card
+              </label>
+            </div>
+            <span className="text-yellow-400">
+              <FaRegCreditCard className="text-5xl" />
+            </span>
           </div>
         </form>
         <div className="flex h-56 w-full flex-col justify-center rounded-md bg-primary/5 pl-12 shadow-md lg:w-2/5">
