2c673f09c5a133322929cbcdcb2185f9002e2028
Kiran Mahajan <thekiranmahajan@outlook.com>
2025-03-16 15:19:16 +0530

    fix: to import.meta.env.MODE instead NODE_ENV

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,5 +1,5 @@
 export const BASE_BACKEND_URL =
-  import.meta.env.NODE_ENV === "production"
+  import.meta.env.MODE === "production"
     ? "https://be-commerce-q7nw.onrender.com"
     : "http://localhost:5000";
 export const PRODUCTS_URL = "/api/products";
