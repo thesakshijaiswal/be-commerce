7b7c591457fec76e6bf82d453ff68b94240d4409
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2025-03-16 19:46:47 +0530

    feat: add order controller with addOrderItems function for creating new orders

---
+++ backend/src/controllers/order.controller.js
@@ -0,0 +1,37 @@
+import asyncHandler from "../utils/helper";
+import Order from "../models/order.model.js";
+
+const addOrderItems = asyncHandler(async (req, res) => {
+  const {
+    orderItems,
+    shippingAddress,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  } = req.body;
+
+  if (orderItems?.length === 0) {
+    res.status(400);
+    throw new Error("No Order Items Found");
+  } else {
+    const order = new Order({
+      orderItems: orderItems.map((item) => ({
+        ...item,
+        product: item.product._id,
+      })),
+      user: req.user._id,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    });
+    const createdOrder = await order.save();
+    res.status(201).json(createdOrder);
+  }
+});
+
+export { addOrderItems };
