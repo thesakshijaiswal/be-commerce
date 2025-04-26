f268ec80348d536904e558730fa99701f43f304f
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2025-04-26 15:44:54 +0530

    style: replace empty cart image and adjust its layout

--- frontend/src/components/EmptyCart.jsx
+++ frontend/src/components/EmptyCart.jsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import Button from "./Button";
-import emptyCartSVG from "../assets/cart.svg";
+import emptyCartSVG from "../assets/empty-cart.svg";
 import { FaLocationArrow } from "react-icons/fa6";
 
 const EmptyCart = () => {
@@ -10,8 +10,7 @@ const EmptyCart = () => {
   };
   return (
     <div className="-mt-14 flex h-screen flex-col items-center justify-center space-y-6 text-center">
-      <div className="absolute h-full w-1/2 rounded-full bg-primary/5 blur-2xl"></div>
-      <img src={emptyCartSVG} className="h-56 md:h-80" alt="Empty cart" />
+      <img src={emptyCartSVG} className="h-60 md:h-96" alt="Empty cart" />
       <h3 className="text-2xl font-bold">Your cart is empty</h3>
       <p className="w-11/12 text-gray-500 md:w-full">
         Looks like you have not added anything to your cart. Go ahead & explore
